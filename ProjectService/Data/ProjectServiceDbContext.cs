d1271e3d1cc1010aa32925ff54a352ffbfaf6fc9
Ronald Barbosa <ronaldverissimo@hotmail.com>
2026-04-14 17:51:57 -0300

    fix: changed migrations directory path

--- ProjectService/Data/ProjectServiceDbContext.cs
+++ ProjectService/Data/ProjectServiceDbContext.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using ProjectService.Domain.Entities;
 
 namespace ProjectService.Data;
@@ -6,4 +8,9 @@ namespace ProjectService.Data;
 public class ProjectServiceDbContext(DbContextOptions<ProjectServiceDbContext>  options) : DbContext(options)
 {
     public DbSet<Project> Projects { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("public");
+    }
 }
